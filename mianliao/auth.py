dcc0af2f0ec2fc891cc9a39ea571d6c5d0da2b25
Pandada8 <pandada8@gmail.com>
2016-02-25 21:13:01 +0800

    fix the mianliao

--- mianliao/auth.py
+++ mianliao/auth.py
@@ -17,7 +17,9 @@ print("""
         \/        \/     \/             \/
 """)
 
-pass_path = os.path.expanduser("~/.cache/wifi_auth/config.json")
+requests.packages.urllib3.disable_warnings()
+
+pass_path = os.path.expanduser("~/.cache/mianliao_auth.json")
 r = requests.session()
 ua = "Mozilla/5.0 (X11; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0"
 r.headers['User-Agent'] = ua
@@ -27,36 +29,23 @@ os.makedirs(os.path.split(pass_path)[0], exist_ok=True)
 if os.path.exists(pass_path):
     with open(pass_path) as fp:
         data = json.load(fp)
-        #r.get("http://wifi.52mianliao.com")
-        r.post('http://wifi.52mianliao.com', data={'ua':ua, 'sh': 720, 'sw':1366, 'ww': 1330, 'wh': 600})
-        r.post("http://wifi.52mianliao.com/", data={
-            "action": "login",
-            "username": data["username"],
-            "password": data["password"]
-        })
-        rc = r.post('http://wifi.52mianliao.com', data={'ua':ua,    'sh': 720, 'sw':1366, 'ww': 1330, 'wh': 600}).content.decode()
-        p = pq(rc)
-        if "登陆用户" in rc:
-            # success
-            print("\n".join([i.text for i in p("label")]))
-        else:
-            print("Login Failed.......")
 else:
-        data = {}
-        data["username"] = input("Username:")
-        data['password'] = getpass.getpass()
-        rc = r.post("http://wifi.52mianliao.com/", data={
-            "action": "login",
-            "username": data["username"],
-            "password": data["password"]
-        }).content.decode()
-        p = pq(rc)
-        if "登陆用户" in rc:
-            # success
-            print("Login Success!")
-            with open(pass_path, 'w') as fp:
-                print("\n".join([i.text for i in p("label")]))
-                json.dump(data, fp)
-        else:
-            print("Login Failed.......")
+    data = {}
+    data["username"] = input("Username: ")
+    data['password'] = getpass.getpass()
 
+r.post('https://wifi.52mianliao.com', data={'ua':ua, 'sh': 720, 'sw':1366, 'ww': 1330, 'wh': 600}, verify=False)
+r.post("https://wifi.52mianliao.com/", data={
+    "action": "login",
+    "username": data["username"],
+    "password": data["password"]
+}, verify=False)
+rc = r.post('https://wifi.52mianliao.com', data={'ua':ua, 'sh': 720, 'sw':1366, 'ww': 1330, 'wh': 600}, verify=False).content.decode()
+if "登陆用户" in rc:
+    p = pq(rc)
+    # success
+    print("\n".join([i.text for i in p("label")]))
+    with open(pass_path, 'w') as fp:
+        json.dump(data, fp)
+else:
+    print("Login Failed.......")
